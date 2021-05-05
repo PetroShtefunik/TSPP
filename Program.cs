8aa6b7fa0ecbe2e527c134486d45d8256d75aa78
Petrik <65122913+PetroShtefunik@users.noreply.github.com>
2021-05-05 23:01:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab2_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double y, x = 0, b = Math.PI, dx = Math.PI/20;
+            Console.WriteLine("  X\t\t Y");
+            do
+            {
+                x = Math.Round(x, 2);
+                
+                if (x == 0)
+                {
+                    Console.WriteLine(Math.Round(x, 2) + "\t\tНеможливо");
+                }
+                else
+                {
+                    y = Math.Tan(x);
+                    y = Math.Round(y, 2);
+                    Console.WriteLine(x + "\t\t" + y);
+
+                }
+                x += dx;
+            } while (x <= b);
+        }
+    }
+}
